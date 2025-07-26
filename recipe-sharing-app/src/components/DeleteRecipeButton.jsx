33c73c8f24fbe7ef04a44c204b8f8b549a77e0c9
Brian <karanjabrian382@gmail.com>
2025-07-27 00:01:09 +0300

    done

--- recipe-sharing-app/src/components/DeleteRecipeButton.jsx
+++ recipe-sharing-app/src/components/DeleteRecipeButton.jsx
@@ -1,4 +1,4 @@
-import { useRecipeStore } from './recipeStore';
+import { useRecipeStore } from './recipeStore.js';
 
 const DeleteRecipeButton = ({ recipeId, onDeleted }) => {
   const deleteRecipe = useRecipeStore(state => state.deleteRecipe);
