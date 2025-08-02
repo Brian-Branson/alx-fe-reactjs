59d2e41461f6ad2da3ca60b9967756a156b18ef3
Brian <karanjabrian382@gmail.com>
2025-08-02 16:06:49 +0300

    done

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,8 +1,8 @@
+import axios from "axios";
+
 export async function fetchUserData(username) {
-  const response = await fetch(`https://api.github.com/users/${encodeURIComponent(username)}`);
-  if (!response.ok) {
-    throw new Error(`GitHub user not found: ${username}`);
-  }
-  const data = await response.json();
-  return data;
+  const response = await axios.get(
+    `https://api.github.com/users/${encodeURIComponent(username)}`
+  );
+  return response.data;
 }
